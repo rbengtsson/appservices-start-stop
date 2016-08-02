a8c482198241066b121d34a24c5b634feeced4b8
Greg Pakes <greg.pakes@isams.co.uk>
2016-08-02 11:52:18 +1000

    Added support for deployment slots.  Inc remented version as per semver. Added convenience build.cmd file.

--- Stop/Stop.ps1
+++ Stop/Stop.ps1
@@ -10,18 +10,43 @@ param
     $ConnectedServiceNameARM,
 
     [String] [Parameter(Mandatory = $true)]
-    $WebAppName
+    $WebAppName,
+    
+    [String]
+    $Slotname
 )
 
-Write-Verbose "Init Stop task"
+Write-Host "Init Stop task"
 
-Write-Verbose "ConnectedServiceNameSelector = $ConnectedServiceNameSelector"
-Write-Verbose "ConnectedServiceName = $ConnectedServiceName"
-Write-Verbose "ConnectedServiceNameARM = $ConnectedServiceNameARM"
-Write-Verbose "WebAppName = $WebAppName"
+Write-Host "ConnectedServiceNameSelector = $ConnectedServiceNameSelector"
+Write-Host "ConnectedServiceName = $ConnectedServiceName"
+Write-Host "ConnectedServiceNameARM = $ConnectedServiceNameARM"
+Write-Host "WebAppName = $WebAppName"
 
-##Initialize-Azure
-$website = Get-AzureWebsite -Name $WebAppName
-Stop-AzureWebsite -Name $WebAppName
+# using production slot for website if website name provided does not contain any slot
+# supports using slot explicitally, or using slot in app name or supplying no slot.
+if ([String]::IsNullOrEmpty($Slotname))
+{
+    if($WebAppName -notlike '*(*)*')
+    {
+        $Slotname  = 'Production'
+    }
+}
 
-Write-Verbose "Ending Stop task"
+Write-Host "Slot = $Slotname"
+
+if ($Slotname){
+    # Slot has been specified
+    Write-Host "##[command]Get-AzureWebsite -Name $WebAppName -Slot $Slotname"
+    $website = Get-AzureWebsite -Name $WebAppName -Slot $Slotname
+    Write-Host "##[command]Stop-AzureWebsite -Name $WebAppName -Slot $Slotname"
+    Stop-AzureWebsite -Name $WebAppName -Slot $Slotname
+}else{
+    # This case the app name contains the slot details
+    Write-Host "##[command]Get-AzureWebsite -Name $WebAppName"
+    $website = Get-AzureWebsite -Name $WebAppName
+    Write-Host "##[command]Stop-AzureWebsite -Name $WebAppName"
+    Stop-AzureWebsite -Name $WebAppName
+}
+
+Write-Host "Ending Stop task"
